726b005721af46f8a6dd8adb3cc9966cd524a364
Roland <185738272+stoaxcode@users.noreply.github.com>
2026-03-02 13:32:14 +0800

    adjust keybinds and create script for mode minimal

---
+++ scripts/mode_minimal.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+hyprctl keyword general:gaps_in 3
+hyprctl keyword general:gaps_out 3
+hyprctl keyword decoration:rounding 4
+hyprctl keyword decoration:blur false
+hyprctl keyword animations:enabled false
